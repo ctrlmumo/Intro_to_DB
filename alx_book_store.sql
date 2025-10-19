861d32709944d31e7c441f8dc9fb0c5967749dd5
Gloria <ctrlmumo11@gmail.com>
2025-10-19 05:30:18 +0300

    Alter both foreign key constraints in Order_Details table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,8 +28,7 @@ CREATE TABLE Order_Details (
 orderdetailid INT PRIMARY KEY,
 order_id INT,
 book_id INT,
-quantity double,
-FOREIGN KEY(order_id) REFERENCES Orders(order_id),
-FOREIGN KEY(book_id) REFERENCES Books(book_id)
+quantity DOUBLE,
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
-SHOW TABLES;
